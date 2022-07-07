e973c2f2437ecb469b8a1699a0b2cc5bbe31f5d5
Dmitry Kalinin <chaqqino@gmail.com>
2022-07-07 15:36:26 +0300

    fixed some little bugs, added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user, optional: true
+
+  validates :title, :body, presence: true
+  validates :title, length: { in: 3..125 }
+  validates :body, length: { in: 3..2000 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name,
+            length: { minimum: 3, maximum: 127 },
+            presence: true,
+            uniqueness: {
+              case_sensitive: false
+            }
 end
