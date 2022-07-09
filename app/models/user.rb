b748546ef61d74143a5198fcb9326f78ed667776
Dmitry Kalinin <chaqqino@gmail.com>
2022-07-09 23:00:38 +0300

    Added raw avatar attachments for user, added edit preferences for user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :posts, dependent: :destroy
+  has_one_attached :avatar, dependent: :destroy
+
+  after_commit :add_default_avatar, on: %i[create update]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -12,4 +15,27 @@ class User < ApplicationRecord
             uniqueness: {
               case_sensitive: false
             }
+
+  def avatar_thumbnail 
+    if avatar.attached?
+      avatar.variant(resize: '150x150!').processed
+    else
+      '/default_avatar.jpg'
+    end
+  end
+
+  private
+
+  def add_default_avatar
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'default_avatar.jpg'
+          )
+        ), filename: 'default_avatar.jpg',
+        content_type: ['image/png', 'image/jpg']
+      )
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module LetsTry
     #
     # config.time_zone = "Moscow"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_storage.variant_processor = :mini_magick
     config.active_record.default_timezone = :local
   end
 end
