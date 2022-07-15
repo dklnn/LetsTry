168b54bd9f18cf053dfff24ad2d8d66cf07b818f
Dmitry Kalinin <chaqqino@gmail.com>
2022-07-15 10:49:38 +0300

    Some comments and little bug with FFaker on RSPec

--- Gemfile
+++ Gemfile
@@ -55,7 +55,7 @@ gem 'sassc-rails'
 
 gem 'mini_magick'
 
-gem 'ffaker', require: false
+gem 'ffaker'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem 'image_processing', '~> 1.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
 
   # self-join association of user model
   # using follow model, user realizing many-to-many connection on it's own
+  # user has many user.followers though follow's table
+  # & user has many user.followees though follow's table
   has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
   has_many :followees, through: :followed_users
   has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
